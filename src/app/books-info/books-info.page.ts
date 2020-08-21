7112db05ef8002f900d449b21a878a8dda595911
Dzifa Atsutse <oladecoolsm>
2020-08-21 18:58:57 +0000

    changes

--- src/app/books-info/books-info.page.ts
+++ src/app/books-info/books-info.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
+import { ActivatedRoute } from '@angular/router';
+
 import { Books } from '../interface/books';
 
 @Component({
@@ -9,14 +11,29 @@ import { Books } from '../interface/books';
 })
 export class BooksInfoPage implements OnInit {
 
+  books: any[] = [];
+  filtered: any[] = [];
+  book: any;
 
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-
+    const id = this.route.snapshot.paramMap.get('id');
+    this.init();
+    this.getBooks();
   }
 
+  getBooks(){
+    this.api.get('books').subscribe((data: any[]) => {
+      console.log(data);
+      this.books = data; this.filtered = data;
+    })  
+  }
 
-
+  init() {
+    this.book = { 
+      title: '', description: '', cover: '', price: '' 
+    };
+  }
 
 }
